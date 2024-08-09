74c9b0aff28d30b7db7e8b0a634bae659a81dc61
muhammaduss <muhammad20040130@gmail.com>
2024-08-10 00:05:20 +0300

    fixing dotfiles not show

--- init.lua
+++ init.lua
@@ -63,3 +63,18 @@ end)
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.relativenumber = true
+
+require("nvim-tree").setup {
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+}
