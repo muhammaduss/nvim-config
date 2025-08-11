c7239c11dfcf035167d42fad72739bd4d2e661a8
muhammaduss <muhammad20040130@gmail.com>
2025-08-11 23:49:36 +0300

    feat: add markdown browser preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -84,6 +84,16 @@ return {
     end,
   },
 
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+
   -- {
   --   "OXY2DEV/markview.nvim",
   --   lazy = false,
