903680934f7ab9bf4f56a869da528b9e00ea5a5e
muhammaduss <muhammad20040130@gmail.com>
2025-09-23 23:07:11 +0300

    refactor: replace deprecated nvim-lspconfig setup API

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,55 +1,40 @@
--- load defaults i.e lua_lsp
+-- Initialize nvim-lspconfig to make its server configs available
+-- This line is still needed for the plugin to add its configs to the runtime path :cite[4]
+require "lspconfig"
+
+-- Load NVChad's default LSP configurations
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+-- Define global defaults for all LSP servers :cite[4]
+local nvlsp = require "nvchad.configs.lspconfig"
+vim.lsp.config("*", {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  flags = nvlsp.lsp_flags,
+})
 
--- EXAMPLE
+-- List of servers to enable
 local servers = {
   "lua_ls",
   "ts_ls",
   "html",
+  "clangd",
   "cssls",
-  -- "phpactor",
-  -- "hls",
   "dockerls",
   "gopls",
   "svelte",
   "basedpyright",
   "marksman",
-  -- "docker_compose_language_service",
 }
-local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+vim.lsp.enable(servers)
 
--- lspconfig.hls.setup {
---   on_attach = function(client, bufnr)
---     client.server_capabilities.documentFormattingProvider = false
---     client.server_capabilities.documentRangeFormattingProvider = false
---     nvlsp.on_attach(client, bufnr)
---   end,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
-
-lspconfig.yamlls.setup {
-  on_attach = function(client, bufnr)
-    -- client.server_capabilities.documentFormattingProvider = true
-    nvlsp.on_attach(client, bufnr)
-  end,
-  flags = nvlsp.lsp_flags,
-  capabilities = nvlsp.capabilities,
+-- For yamlls
+vim.lsp.config("yamlls", {
   settings = {
     yaml = {
       schemas = {
-        -- ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.33.0-standalone-strict/all.json"] = "k8s-*.yaml",
         kubernetes = { "k8s.yaml", "templates/*" },
         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
@@ -61,13 +46,13 @@ lspconfig.yamlls.setup {
       },
     },
   },
-}
+})
 
+-- For clangd with custom on_attach
 local custom_on_attach = function(client, bufnr)
   require("nvchad.configs.lspconfig").on_attach(client, bufnr)
 
   local opts = { noremap = true, silent = true, buffer = bufnr }
-
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
@@ -76,35 +61,15 @@ local custom_on_attach = function(client, bufnr)
   vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 end
 
-lspconfig.clangd.setup {
+vim.lsp.config("clangd", {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
     custom_on_attach(client, bufnr)
   end,
-  capabilities = nvlsp.capabilities,
-}
+})
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
-
--- require("lspconfig").phpactor.setup {
---   root_dir = function(_)
---     return vim.loop.cwd()
---   end,
---   init_options = {
---     ["language_server.diagnostics_on_update"] = false,
---     ["language_server.diagnostics_on_open"] = false,
---     ["language_server.diagnostics_on_save"] = false,
---     ["language_server_phpstan.enabled"] = false,
---     ["language_server_psalm.enabled"] = false,
---   },
--- }
---
-require("lspconfig").lua_ls.setup {
+-- For lua_ls with custom runtime path detection
+vim.lsp.config("lua_ls", {
   on_init = function(client)
     local path = client.workspace_folders[1].name
     if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
@@ -113,25 +78,17 @@ require("lspconfig").lua_ls.setup {
 
     client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
       runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
       },
-      -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
         library = {
           vim.env.VIMRUNTIME,
-          -- Depending on the usage, you might want to add additional paths here.
-          -- "${3rd}/luv/library"
-          -- "${3rd}/busted/library",
         },
-        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-        -- library = vim.api.nvim_get_runtime_file("", true)
       },
     })
   end,
   settings = {
     Lua = {},
   },
-}
+})
